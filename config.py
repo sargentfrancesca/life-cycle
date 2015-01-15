06631576ae3b012824fa9f14af02c2f3204f44f2
sargentfrancesca <sargentfrancesca@gmail.com>
2015-01-15 15:59:33 +0000

    Deleting stuff

--- config.py
+++ config.py
@@ -23,18 +23,18 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'mysql://root:jeh5t@localhost/lifecycle'
+        'mysql://root:your_password@localhost/lifecycle'
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'mysql://root:jeh5t@localhost/lifecycle'
+        'mysql://root:your_password@localhost/lifecycle'
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql://root:jeh5t@localhost/lifecycle'
+        'mysql://root:your_password@localhost/lifecycle'
 
 
 config = {
