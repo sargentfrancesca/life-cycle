5526c2dab2b02ba126129db3149083b34ebb1707
Francesca Sargent <f.sargent@exeter.ac.uk>
2015-01-16 11:43:35 +0000

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'sargentfrancesca'
-    MAIL_PASSWORD = 'm0nkeywrench'
+    MAIL_USERNAME = 'username'
+    MAIL_PASSWORD = 'password'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Life Cycle]'
-    FLASKY_MAIL_SENDER = 'Life Cycle Admin <sargentfrancesca@gmail.com>'
+    FLASKY_MAIL_SENDER = 'Life Cycle Admin <you@email.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
 
@@ -23,18 +23,18 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'mysql://root:jeh5t@localhost/lifecycle'
+        'mysql://user:pass@localhost/lifecycle'
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'mysql://root:jeh5t@localhost/lifecycle'
+        'mysql://user:pass@localhost/lifecycle'
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql://root:jeh5t@localhost/lifecycle'
+        'mysql://user:pass@localhost/lifecycle'
 
 
 config = {
