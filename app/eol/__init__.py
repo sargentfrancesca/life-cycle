59dc53626f9315b66b8f46bc7576fcceb7146205
Francesca <francesca@ARDUsers-iMac.local>
2015-02-05 14:15:50 +0000

    removing errory stuff

--- app/eol/__init__.py
+++ app/eol/__init__.py
@@ -2,10 +2,3 @@ from flask import Blueprint
 
 eol = Blueprint('eol', __name__)
 
-from . import views, errors
-from ..models import Permission, Plant, Species
-
-
-@demography.app_context_processor
-def inject_permissions():
-    return dict(Permission=Permission)
